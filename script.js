d7e8bde1a76739aa3c3a580b32687e2e3fbf7762
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-06 10:25:43 +0300

    inheritance basics

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 class Triangle {
   constructor(a, b) {
-    if (!Number.isFinite(a) || a <= 0) throw new Error(`Invalid a: ${a}`);
-
-    if (!Number.isFinite(b) || b <= 0) throw new Error(`Invalid b: ${b}`);
-
     this.a = a;
     this.b = b;
   }
+
   getArea() {
     return (this.a * this.b) / 2;
   }
@@ -15,10 +12,30 @@ class Triangle {
     return Math.sqrt(this.a ** 2 + this.b ** 2);
   }
 
-  triAngleDescription() {
-    return `This triangle do have ${this.getArea()} br^2 area, and hypotenuse is ${this.getHypotenuse()} br`;
+  describe() {
+    return `I am a triangle with area of ${this.getArea()}`;
   }
 }
 
-const firstTriAngle = new Triangle(3, 4);
-console.log(firstTriAngle.triAngleDescription()); // This triangle do have 6, and hypotenuse is 5
+// child class -> ShyTriangle
+// parent class -> Triangle
+class ShyTriangle extends Triangle {
+  describe() {
+    return "(runs and hides)";
+  }
+
+  beShy() {
+    return "Too shyyyy";
+  }
+}
+
+const regularTri = new Triangle(5, 12);
+
+const shyTri = new ShyTriangle(3, 4);
+console.log(shyTri); // ShyTriangle {a: 3, b: 4}
+console.log(shyTri.getArea()); // 6
+console.log(shyTri.getHypotenuse()); // 5
+console.log(shyTri.describe()); // (runs and hides)
+console.log(regularTri.describe()); // I am a triangle with area of 30
+// console.log(regularTri.beShy()); // Uncaught TypeError: regularTri.beShy is not a function
+console.log(shyTri.beShy()); // Too shyyyy
