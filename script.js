f7ff83d219cc18b06fbff9bb487e2dbad9a22c65
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-06 09:35:46 +0300

    class basics

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ let myTri = {
 
 console.log(myTri.getHypotenuse()); // 5
 // It works end of the day but If I had 10.000 triangles it wouldn't work at all. Do I have to write bunch of myTri, myTri1, myTri2, ... myTri1000 ? lol, HELL NOOOO. So solution is just there, Classes.
+
+class Triangle {
+  getArea() {
+    return (this.a * this.b) / 2;
+  }
+
+  getHypotenuse() {
+    return Math.sqrt(this.a ** 2 + this.b ** 2);
+  }
+  // this keyword'u classın kendisi olan Triangle'ı işaret etmiyor. Yaratacağım her örneğin kendisini işaret ediyor.
+}
+
+const firstTriangle = new Triangle();
+// this -> firstTriangle
+firstTriangle.a = 3;
+firstTriangle.b = 4;
+console.log(firstTriangle.getHypotenuse()); // 5
+
+const secondTriangle = new Triangle();
+// this -> secondTriangle
+secondTriangle.a = 8;
+secondTriangle.b = 15;
+console.log(secondTriangle.getHypotenuse()); // 17
+
+console.log(typeof secondTriangle); // object
+console.log(firstTriangle instanceof Triangle); // true
