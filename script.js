27588ce4cf2159838970053ff1ab57bc6ee08fa8
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-09 23:04:09 +0300

    use cases for static methods

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-// Static methods
-// ->method is called on a Class, not an object -therefore it cannot have access to individual object attributes
+// Example 1
+class MyMath {
+  static add(a, b) {
+    return a + b;
+  }
+  static multipy(a, b) {
+    return a * b;
+  }
+}
+console.log(MyMath.add(2, 3)); // 5
+console.log(MyMath.multipy(5, 2)); // 10
 
+// Example 2
 class Cat {
   constructor(name, breed) {
     this.name = name;
     this.breed = breed;
   }
+  meow() {
+    return `${this.name} says meow`;
+  }
 
-  static meow() {
-    return "MEOW MEOW MEOW!!";
+  static registerStray() {
+    const names = [
+      "Muffin",
+      "Biscuits",
+      "Sleepy",
+      "Dodo",
+      "Princess Butterface",
+    ];
+    const name = choice(names);
+    return new Cat(name, "unknown");
   }
 }
 
-const blue = new Cat("blue", "scottish");
-console.log(Cat.meow()); // MEOW MEOW MEOW!!
-// console.log(blue.meow());
-// script.js:17 Uncaught TypeError: blue.meow is not a function
+function choice(arr) {
+  const randIdx = Math.floor(Math.random() * arr.length);
+  return arr[randIdx];
+}
+
+// Kedi isimlerinden rastgele birini seçip yeni bi instantiate oluşturucak
+console.log(Cat.registerStray()); // Cat {name: 'Muffin', breed: 'unknown'}
