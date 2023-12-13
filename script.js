a4bf9aacb79f83bec85564a4fb29f619a8b53f94
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-13 21:22:44 +0300

    getters

--- script.js
+++ script.js
@@ -1,69 +1,23 @@
-// Example 1
-class MyMath {
-  static add(a, b) {
-    return a + b;
-  }
-  static multipy(a, b) {
-    return a * b;
-  }
-}
-console.log(MyMath.add(2, 3)); // 5
-console.log(MyMath.multipy(5, 2)); // 10
-
-// Example 2
-class Cat {
-  constructor(name, breed) {
-    this.name = name;
-    this.breed = breed;
-  }
-  meow() {
-    return `${this.name} says meow`;
+// Getters
+class Circle {
+  constructor(radius) {
+    this._radius = radius;
   }
 
-  static registerStray() {
-    const names = [
-      "Muffin",
-      "Biscuits",
-      "Sleepy",
-      "Dodo",
-      "Princess Butterface",
-    ];
-    const name = choice(names);
-    return new Cat(name, "unknown");
+  // Getter for the diameter
+  get diameter() {
+    return this._radius * 2;
+  }
+
+  get radius() {
+    console.log(this._radius);
   }
 }
 
-function choice(arr) {
-  const randIdx = Math.floor(Math.random() * arr.length);
-  return arr[randIdx];
-}
+const circle = new Circle(5);
+console.log(circle.diameter);
+// 10
+// -> Long story in short -> diameter fonksiyon olmasına rağmen get yazdığım için (getter olduğu için) property gibi davranıyor. Bu yüzden parantez yazıp çağırmama gerek yok, normal string gibi sadece property
 
-// Kedi isimlerinden rastgele birini seçip yeni bi instantiate oluşturucak
-console.log(Cat.registerStray()); // Cat {name: 'Muffin', breed: 'unknown'}
-
-// ArrayUtils exercise
-/*
-Implement a class named ArrayUtils that cannot be instantiated and contains static methods average and max. If the class is instantiated throw an error with the message 'ArrayUtils cannot be instantiated.'
-
-
-
-The average method should return the average of an array of numbers. If the array is empty, throw an error with the message 'Array cannot be empty.'
-
-
-
-The max method should return the largest number from an array of numbers. You can assume you will always get passed an arrray of numbers
-*/
-class ArrayUtils {
-  constructor() {
-    throw new Error("ArrayUtils cannot be instantiated.");
-  }
-  static average(arr) {
-    let x = 0;
-    if (arr.length === 0) throw new Error("Array cannot be empty.");
-    return arr.reduce((sum, num) => sum + num, 0) / arr.length;
-  }
-
-  static max(arr) {
-    return Math.max(...arr);
-  }
-}
+circle.radius;
+// 5
