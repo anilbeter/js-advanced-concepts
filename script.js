603a5ed5ef82be634bf94a822f58e1a0e95e8653
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-13 22:24:21 +0300

    setters

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-// Getters
-class Circle {
-  constructor(radius) {
-    this._radius = radius;
-  }
+// Setters
 
-  // Getter for the diameter
-  get diameter() {
-    return this._radius * 2;
+class Circle {
+  static allowedColors = new Set(["red", "green", "blue"]);
+  constructor(radius, color) {
+    this._radius = radius;
+    this._color = color;
   }
 
   get radius() {
-    console.log(this._radius);
+    return this._radius;
+  }
+
+  set radius(value) {
+    if (value < 0) {
+      throw new Error("Radius cannot be negative!");
+    } else {
+      this._radius = value;
+    }
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    if (allowedColors.has(newColor)) {
+      this._color = newColor;
+    } else {
+      throw new Error("That color is not allowed!");
+    }
   }
 }
 
-const circle = new Circle(5);
-console.log(circle.diameter);
-// 10
-// -> Long story in short -> diameter fonksiyon olmasına rağmen get yazdığım için (getter olduğu için) property gibi davranıyor. Bu yüzden parantez yazıp çağırmama gerek yok, normal string gibi sadece property
-
-circle.radius;
-// 5
+const circle = new Circle(5, "red");
+// circle.radius = -3;
+// Uncaught Error: Radius cannot be negative!
+console.log(circle.color); // red
+circle.color = "black"; // script.js:30 Uncaught Error: That color is not allowed!
